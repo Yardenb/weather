9bda178d1dc8dbbd7aa5e1e988ecc0964c775634
yarden <yardenbesh@gmail.com>
2020-01-18 12:34:06 +0200

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,12 @@
-console.log('Client side js file is loaded')
-
-
 const weatherForm = document.querySelector('form');
 const searchElement = document.querySelector('input');
 const msgOne = document.querySelector('#message-1');
 const msgTwo = document.querySelector('#message-2');
 
-
-
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const location = searchElement.value;
-    const URL = `http://localhost:3000/weather?address=${location}`;
+    const URL = `/weather?address=${location}`;
 
     msgOne.textContent = 'Loading...'
     msgTwo.textContent = ''
